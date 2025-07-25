08f128b365b49d9bcc846495381653096ecba1b5
Rajeev kumar <52482675+Rajeev-Kumar-DSA@users.noreply.github.com>
2025-07-25 19:23:05 +0000

    date

---
+++ basics_01/07_date.js
@@ -0,0 +1,38 @@
+let myDate = new Date()
+
+// console.log(typeof myDate)  //Object
+
+// console.log(myDate);  // 2025-07-25T19:07:08.288Z
+
+// console.log(myDate.toString());  // convert in string -  Fri Jul 25 2025 19:08:29 GMT+0000 (Coordinated Universal Time)
+
+// console.log(myDate.toDateString());  // Fri Jul 25 2025
+
+// console.log(myDate.toISOString());   // 2025-07-25T19:08:29.472Z
+
+// console.log(myDate.toLocaleString());   // 7/25/2025, 7:08:29 PM
+
+
+// let myCreatedDate = new Date(2023, 0, 23)  // month count start from 0 index
+// console.log(myCreatedDate.toDateString());  // Mon Jan 23 2023
+
+// let myCreatedDate2 = new Date(2023, 0, 23, 5, 3) 
+// console.log(myCreatedDate2.toLocaleString());   // 1/23/2023, 5:03:00 AM
+
+// let myCreatedDate3 = new Date("2023-01-14") 
+// console.log(myCreatedDate3.toLocaleString());  // 1/14/2023, 12:00:00 AM
+
+// let myCreatedDate4 = new Date("01-14-2023") 
+// console.log(myCreatedDate3.toLocaleString());  // 1/14/2023, 12:00:00 AM
+
+// let myTimeStamp = Date.now() // value come in milisecond
+// console.log(myTimeStamp);  
+// console.log(myCreatedDate4.getTime());
+
+// // change in second
+// console.log(Math.floor(Date.now()/1000))  // always use the floor value
+
+
+
+
+
